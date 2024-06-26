8b56a31fe72dc2ddd352bc5713a73f720cea6ed2
DevKinou <killian.pereira88@gmail.com>
2024-06-26 14:28:44 +0200

    Ajout du endpoint categories/{categorie}/questions

--- NevatwoAPI/NevatwoAPI/Controllers/ListEndpointController.cs
+++ NevatwoAPI/NevatwoAPI/Controllers/ListEndpointController.cs
@@ -16,13 +16,26 @@ namespace NevatwoAPI.Controllers
             _context = context;
         }
 
+        // Liste des grilles
+        //[HttpGet("grilles")]
+        //public IActionResult GetGrilles()
+        //{
+        //}
+
+        //Liste des axes d'une grille
+        //[HttpGet("grilles/{id}/axes")]
+        //public IActionResult GetAxesSinceGrilles (int id)
+        //{
+        //}
+
+        //Liste des catégories d'un axe
         [HttpGet("axes/{id}/categories")]
-        public IActionResult GetCategoriesSinceAxes(int id)
+        public IActionResult GetCategoriesSinceAxeId(int id)
         {
             logger.Info($"Une demande de récupération des catégories pour l'axe {id} a été effectuée");
             List<string?> listCategories = _context.Questions.Where(c => c.axe == id).Select(c => c.item).Distinct().ToList();
 
-            if(listCategories.Count == 0)
+            if (listCategories.Count == 0)
             {
                 logger.Warn($"Aucune catégorie n'a été trouvée pour l'axe {id}");
                 return NotFound();
@@ -30,5 +43,27 @@ namespace NevatwoAPI.Controllers
 
             return Ok(listCategories);
         }
+
+        //Ensemble des questions d'une catégorie
+        [HttpGet("categories/{categorie}/questions")]
+        public IActionResult GetQuestionsSinceCategorie(string categorie)
+        {
+            logger.Info($"Une demande de récupération des questions pour la catégorie {categorie} a été effectuée");
+            List<string?> listQuestions = _context.Questions.Where(c => c.item.Contains(categorie)).Select(c => c.libelle).Distinct().ToList();
+
+            if (listQuestions.Count == 0)
+            {
+                logger.Warn($"Aucune question n'a été trouvée pour la catégorie {categorie}");
+                return NotFound();
+            }
+
+            return Ok(listQuestions);
+        }
+
+        //[HttpGet("/search/questions?q=query")]
+        //public IActionResult GetQuestionsSinceQuery(string query)
+        //{
+        //   
+        //}
     }
 }

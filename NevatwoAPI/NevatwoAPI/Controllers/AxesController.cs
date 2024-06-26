217ce1db34b616b3435392ca5b694e30c5f8213c
DevKinou <killian.pereira88@gmail.com>
2024-06-26 11:08:34 +0200

    Ajout du controller "Axes"

---
+++ NevatwoAPI/NevatwoAPI/Controllers/AxesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NevatwoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AxesController : ControllerBase
+    {
+    }
+}

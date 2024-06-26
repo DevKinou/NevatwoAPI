75ae999d11d4b695bff85754f5e85ca12b211fad
DevKinou <killian.pereira88@gmail.com>
2024-06-26 13:42:36 +0200

    Refacto

--- NevatwoAPI/NevatwoAPI/Controllers/ListEndpointController.cs
+++ NevatwoAPI/NevatwoAPI/Controllers/ListEndpointController.cs
@@ -4,23 +4,30 @@ using NLog;
 
 namespace NevatwoAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/")]
     [ApiController]
-    public class AxesController : ControllerBase
+    public class ListEndpointController : ControllerBase
     {
         private readonly NevaTwoDbContext _context;
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
-        public AxesController(NevaTwoDbContext context)
+        public ListEndpointController(NevaTwoDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet("{id}/categories")]
+        [HttpGet("axes/{id}/categories")]
         public IActionResult GetCategoriesSinceAxes(int id)
         {
             logger.Info($"Une demande de récupération des catégories pour l'axe {id} a été effectuée");
             List<string?> listCategories = _context.Questions.Where(c => c.axe == id).Select(c => c.item).Distinct().ToList();
+
+            if(listCategories.Count == 0)
+            {
+                logger.Warn($"Aucune catégorie n'a été trouvée pour l'axe {id}");
+                return NotFound();
+            }
+
             return Ok(listCategories);
         }
     }
